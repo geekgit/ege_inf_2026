34a57d66713fd61a478a3e698338dbe17465d81a
geekgit <geeekz@gmail.com>
2026-09-23 23:26:05 +0500

    Create table_e16_v11.py

---
+++ 16/v11/table_e16_v11.py
@@ -0,0 +1,14 @@
+elements = 6000 # максимальное количество элементов в массиве
+table = [] # таблица значений, будет списком
+for i in range (0, elements):
+    table.insert(i,int(0)) # заполняем список нулями
+
+table[1] = 1 # F(1) = 1
+table[2] = 2 # F(2) = 2
+for i in range (3,elements):
+    table[i] = i * (i - 1) + table[i - 1] + table[i - 2]
+
+
+result = table[2024] - table[2022] - 2 * table[2021] - table[2020]
+print("Результат: ", result)
+
