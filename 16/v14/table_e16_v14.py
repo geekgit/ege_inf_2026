66003adc23ae7d697195c761bf3e7dc885b1c7f8
geekgit <geeekz@gmail.com>
2026-09-23 23:27:42 +0500

    Create table_e16_v14.py

---
+++ 16/v14/table_e16_v14.py
@@ -0,0 +1,17 @@
+elements = 6000 # максимальное количество элементов в массиве
+table = [] # таблица значений, будет списком
+for i in range (0, elements):
+    table.insert(i,int(0)) # заполняем список нулями
+
+table[0] = 0 # F(0) = 0
+table[1] = 1 # F(1) = 1
+table[2] = 2 # F(2) = 2
+for i in range (3,elements):
+    if i % 2 == 0:
+        table[i] = 3 * (i - 1) + table[i - 1] + 5
+    else:
+        table[i] = 3 * (i + 1) + table[i - 2] - 2
+
+result = table[35]
+print("Результат: ", result)
+
